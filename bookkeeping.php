4f53e6a749464e74a31c8271c00f4a4d8f6d8d74
minjie Chiou <q1234565701@gmail.com>
2022-01-08 03:19:16 +0800

    成功實作雙層下拉式選單

--- bookkeeping.php
+++ bookkeeping.php
@@ -37,23 +37,66 @@ else{
     金  額 ：
     <input type="text" name="total"><br/><br/>
     收  支 :
-    <select name="ways" type="submit">
+    <select id="type_list" name="ways" onchange="changeCollege(this.selectedIndex)">>
         <option value="in">收入</option>
         <option value="out" selected>支出</option>
     </select><br/><br/>
     用  途 ：
-    <select name="used">
+    <select id="category_list" name="used">
+        <!--預設進來時是支出，類別選單用支出的-->
         <?php
-            $query = ("SELECT * FROM category where type = ?");
-            $stmt = $db->prepare($query);
-            $stmt->execute(array(false));
-            $result = $stmt->fetchAll();
-            for($i = $stmt->rowCount()-1; $i >=0;$i--){
-                $temp = $result[$i]['category'];
-                echo"<option>$temp</option>";
-            }
+        $inner = "";
+        $query = ("SELECT * FROM category where type = ?");
+        $stmt = $db->prepare($query);
+        $stmt->execute(array(false));
+        $result = $stmt->fetchAll();
+        for ($i = $stmt->rowCount() - 1; $i >= 0; $i--) {
+            echo "<option>".$result[$i]['category']."</option>" ;
+        }
         ?>
     </select><br/><br/>
+
+    <script type="text/javascript">
+        var typeSelect=document.getElementById("type-list");
+        var categorySelect=document.getElementById("category-list");
+        //收支選單的選取值改變時，改變類別選單
+        function changeCollege(index){
+            var inner;
+            if(index == 1) {
+                <!--支出(type=0)-->
+                <?php
+                    $inner = "";
+                    $query = ("SELECT * FROM category where type = ?");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute(array(false));
+                    $result = $stmt->fetchAll();
+                    for ($i = $stmt->rowCount() - 1; $i >= 0; $i--) {
+                        $inner = $inner."<option>".$result[$i]['category']."</option>" ;
+                    }
+                ?>
+                inner="<?php echo $inner; ?>";
+            }
+            else{
+                <!--收入(type=1)-->
+                <?php
+                    $inner = "";
+                    $query = ("SELECT * FROM category where type = ?");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute(array(true));
+                    $result = $stmt->fetchAll();
+                    for($i = $stmt->rowCount()-1; $i >=0;$i--){
+                        $inner = $inner."<option>".$result[$i]['category']."</option>" ;
+                    }
+                ?>
+                inner="<?php echo $inner; ?>";
+            }
+            //改動類別選單的內容
+            var categorySelect=document.getElementById("category_list");
+            categorySelect.innerHTML=inner;
+        }
+        changeCollege(document.getElementById("type-list").selectedIndex);
+    </script>
+
     描  述:
     <input type="text" name="dec"><br/><br/>
     <input type="submit" value="確認" name="check">
